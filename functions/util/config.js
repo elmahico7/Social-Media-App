b1715ccaf582682ceda444711f32e0ed638f80e0
elmahico7 <53228477+elmahico7@users.noreply.github.com>
2020-06-23 19:44:01 +0300

    socialapp-functions

---
+++ functions/util/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apiKey: "AIzaSyD_8HHov14YrOsDq74JjZvL11_zlm-TW3Y",
+    authDomain: "socialapp-6f6ba.firebaseapp.com",
+    databaseURL: "https://socialapp-6f6ba.firebaseio.com",
+    projectId: "socialapp-6f6ba",
+    storageBucket: "socialapp-6f6ba.appspot.com",
+    messagingSenderId: "186865444151",
+    appId: "1:186865444151:web:ea29b1458d894d41bce6b2",
+    measurementId: "G-7FWPFJQT0Y"
+  
+};
+
